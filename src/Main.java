49330b5d0fda224c3905293d6c35d60131ced3fe
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-05 14:03:47 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a names student");
+        String name = scanner.nextLine();
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(name)) {
+                System.out.println("Vị trí thành viên trong danh sách " + name + " là " + i);
+                isExist =  true;
+                break;
+            }
+        }
+        if (!isExist) {
+            System.out.println("Không tìm thấy" + name + "trong danh sách");
+        }
+    }
 }
